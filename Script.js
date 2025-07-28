9fb4014337bd3cd3768a0159c8c91b7546f50c09
oskar <oskaraszinecnko500@gmail.com>
2025-07-28 16:33:40 -0400

    added most of the base main stuff will have to work on graphics and smaller functional things next

---
+++ Script.js
@@ -0,0 +1,13 @@
+//big background n stuff
+const bodyElement = document.body;
+
+//setting up my buttons in JS FORM!!!!!
+const buttonProjects = document.getElementById("button-proj");
+const buttonAboutme = document.getElementById("button-abt");
+const buttonContact = document.getElementById("button-cont");
+const buttonLinks = document.getElementById("button-link");
+
+//hover over buttons event listener :o
+
+
+
